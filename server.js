1ace1eac67c1f53c5c9342baaf5f15f3baa4d731
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-13 16:23:39 +0200

    finito

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
+const fantasy = require("./fantasy.json");
+const history = require("./history.json");
+const horror = require("./horror.json");
+const romance = require("./romance.json");
+const scifi = require("./scifi.json");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/hello", (req, res) => {
+  res.send(">>>>>>>>>>>>>>>> Hello <<<<<<<<<<<<<<<<<<<<<");
+});
+
+app.get("/fantasy", (req, res) => {
+  res.send(require("./fantasy.json"));
+});
+
+app.get("/history", (req, res) => {
+  res.send(require("./history.json"));
+});
+
+app.get("/horror", (req, res) => {
+  res.send(require("./horror.json"));
+});
+
+app.get("/romance", (req, res) => {
+  res.send(require("./romance.json"));
+});
+
+app.get("/scifi", (req, res) => {
+  res.send(require("./scifi.json"));
 });
 
 app.listen(port, () => {
